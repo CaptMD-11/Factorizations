9bd890f19f02d75ff7cd78f9e0752e1b6af8117f
Vignesh Nydhruva <nydhruva@wifi-10-45-196-158.wifi.berkeley.edu>
2024-01-21 22:46:18 -0800

    finished getCombinationsNoDuplicates

--- src/Factorizations.java
+++ src/Factorizations.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Factorizations {
 
+    private int n;
     private ArrayList<Integer> primeFactorization;
     private ArrayList<ArrayList<Integer>> allFactorizationsWithDuplicates;
 
-    public Factorizations() {
-        primeFactorization = new ArrayList<Integer>(); 
+    public Factorizations(int inputN) {
+        n = inputN;
+        primeFactorization = new ArrayList<Integer>();
+        primeFactorization = getPrimeFactorization(n); 
         allFactorizationsWithDuplicates = new ArrayList<ArrayList<Integer>>(); 
     }
 
@@ -17,15 +21,103 @@ public class Factorizations {
         
     // }
 
-    // // returns all possible factorizations of n (includes duplicates)
-    // // input is an ArrayList<Integer> that represents the prime factorization of n
-    // public ArrayList<ArrayList<Integer>> getAllFactorizationsWithDuplicates(ArrayList<Integer> primeFactorization) {
-    //     if ()
+    // /*
+    //  * returns all possible factorizations of n (includes duplicates)
+    //  * returns the member variable allFactorizationsWithDuplicates
+    //  * input is an ArrayList<Integer> that represents the prime factorization of n
+    //  */
+    // public ArrayList<ArrayList<Integer>> getAllFactorizationsWithDuplicates(ArrayList<Integer> factorization) {
+    //     if (factorization.size() == 2) {
+    //         allFactorizationsWithDuplicates.add(factorization); 
+    //     } else {
+    //         ArrayList<Integer> nextLevelFactorization = new 
+    //     }
 
     // }
 
-    // helper method
-    // returns the product of all the integers in a list 
+    /*
+     * helper method
+     * STEP 1 OF ALGORITHM
+     * returns all combinations (grouped into pairs of 2) of a factorization with no duplicates 
+     */
+    public ArrayList<ArrayList<Integer>> getCombinationsNoDuplicates() {
+        ArrayList<ArrayList<Integer>> res1 = new ArrayList<ArrayList<Integer>>(); 
+        int numFactors = primeFactorization.size();
+            for (int j = 0; j < numFactors-1; j++) {
+                for (int k = j+1; k < numFactors; k++) {
+                    ArrayList<Integer> pair = new ArrayList<Integer>();
+                    pair.add(primeFactorization.get(j));
+                    pair.add(primeFactorization.get(k));
+                    res1.add(pair);
+                }
+            }
+        ArrayList<ArrayList<Integer>> res2 = new ArrayList<ArrayList<Integer>>(); 
+        res2.add(res1.get(0)); 
+        for (int i = 1; i < res1.size(); i++) {
+            if (firstListExistsInSecondList(res1.get(i), res2) == false)
+                res2.add(res1.get(i)); 
+        }
+        return res2;
+    }
+
+    /*
+     * helper method
+     * returns true if list1 is found in list2 and false otherwise 
+     */
+    public boolean firstListExistsInSecondList(ArrayList<Integer> list1, ArrayList<ArrayList<Integer>> list2) {
+        String list1Str = "";
+        for (int i = 0; i < list1.size(); i++) {
+            list1Str += list1.get(i) + " "; 
+        }
+        list1Str = list1Str.trim();
+        for (int i = 0; i < list2.size(); i++) {
+            String tempStr = ""; 
+            for (int j = 0; j < list2.get(i).size(); j++) {
+                tempStr += list2.get(i).get(j) + " "; 
+            }
+            tempStr = tempStr.trim(); 
+            if (list1Str.equals(tempStr))
+                return true;
+        }
+        return false; 
+    }
+
+    /*
+     * helper method
+     * returns true if both the lists are the same (doesn't matter if they are sorted or not) and false otherwise 
+     */
+    public boolean areSameList(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        list1 = sort(list1); 
+        list2 = sort(list2); 
+        if (list1.size() != list2.size()) 
+            return false; 
+        for (int i = 0; i < list1.size(); i++) {
+            if (list1.get(i) != list2.get(i))
+                return false;
+        }
+        return true; 
+    }
+
+    /*
+     * helper method
+     * returns a sorted version of the input ArrayList in increasing order 
+     */
+    public ArrayList<Integer> sort(ArrayList<Integer> list) {
+        int[] arr = new int[list.size()]; 
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i); 
+        }
+        Arrays.sort(arr); 
+        for (int i = 0; i < arr.length; i++) {
+            list.set(i, arr[i]); 
+        }
+        return list; 
+    }
+
+    /*
+     * helper method
+     * returns the product of all the integers in a list 
+     */
     public int product(ArrayList<Integer> factorization) {
         int res = 1;
         for (int i = 0; i < factorization.size(); i++) {
@@ -34,8 +126,10 @@ public class Factorizations {
         return res; 
     }
 
-    // helper method
-    // returns an ArrayList<Integer> that represents the prime factorization of n
+    /*
+     * helper method
+     * returns an ArrayList<Integer> that represents the prime factorization of n
+     */
     public ArrayList<Integer> getPrimeFactorization(int n) {
         if (n==1) {
             return primeFactorization;
@@ -50,8 +144,10 @@ public class Factorizations {
         
     }
 
-    // helper method
-    // returns true if n is prime and false otherwise 
+    /*
+     * helper method
+     * returns true if n is prime and false otherwise
+     */
     public boolean isPrime(int n) {
         for (int i = 2; i <= n/2; i++) {
             if (n % i == 0)
@@ -60,8 +156,10 @@ public class Factorizations {
         return true; 
     }
 
-    // helper method
-    // returns least factor of n
+    /*
+     * helper method
+     * returns least factor of n
+     */
     public int getLeastFactor(int n) {
         for (int i = 2; i <= n/2; i++) {
             if (n % i == 0)
--- src/Runner.java
+++ src/Runner.java
@@ -3,14 +3,12 @@ import java.util.ArrayList;
 public class Runner {
     public static void main(String[] args) throws Exception {
 
-        Factorizations obj = new Factorizations();
-        // System.out.println(obj.isPrime(659));
+        Factorizations obj = new Factorizations(100);
+         System.out.println(obj.isPrime(659));
         // System.out.println(obj.getPrimeFactorization(102));
-        ArrayList<Integer> list = new ArrayList<Integer>(); 
-        list.add(2);
-        list.add(3);
-        list.add(17);
-        System.out.println(obj.product(list));
+        
+
+        System.out.println(obj.getCombinationsNoDuplicates());
 
     }
 }
