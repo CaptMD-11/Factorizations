bf55a814f65b23336e77e7a37aaa73ac7aeca3fc
Vignesh Nydhruva <nydhruva@wifi-10-45-196-158.wifi.berkeley.edu>
2024-01-27 23:47:55 -0800

    safe bet

--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Runner {
     public static void main(String[] args) throws Exception {
 
-        Factorizations obj = new Factorizations(100);
+        Factorizations obj = new Factorizations(100);//
         ArrayList<ArrayList<Integer>> factorizations = obj.getFirstLevelFactorizations();
         System.out.println(obj.getAllFactorizationsWithDuplicates(factorizations));
 
