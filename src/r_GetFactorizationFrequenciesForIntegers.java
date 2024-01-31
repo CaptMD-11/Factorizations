693d5691fd408581547de0edc52eebe54d4ac45f
Vignesh Nydhruva <nydhruva@wifi-10-45-197-101.wifi.berkeley.edu>
2024-01-30 23:25:19 -0800

    added text file writing

--- src/r_GetFactorizationFrequenciesForIntegers.java
+++ src/r_GetFactorizationFrequenciesForIntegers.java
@@ -1,17 +1,46 @@
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 public class r_GetFactorizationFrequenciesForIntegers {
 
     public static void main(String[] args) {
 
-        // input upper bound in below line
-        int upperBound = 500;
+        // input lower bound and upper bound in below lines
+        int lowerBound = 8500;
+        int upperBound = 10000;
         ArrayList<ArrayList<Integer>> factorizationFrequenciesPerNumber = new ArrayList<ArrayList<Integer>>();
-        factorizationFrequenciesPerNumber = getFactorizationFrequenciesPerNumber(upperBound);
+        factorizationFrequenciesPerNumber = getFactorizationFrequenciesPerNumber(lowerBound, upperBound);
 
         // prints out ArrayLists, where each ArrayList = [c,f]
         // c is the composite integer of interest
         // f is the number of distinct factorizations of c
+        try {
+            ArrayList<String> factorizationFrequenciesPerNumberStrList = new ArrayList<String>();
+            for (int i = 0; i < factorizationFrequenciesPerNumber.size(); i++) {
+                factorizationFrequenciesPerNumberStrList.add(factorizationFrequenciesPerNumber.get(i) + "");
+            }
+            File myObj = new File("frequenciesPerInteger.txt");
+            myObj.createNewFile();
+            FileWriter fileWriter = new FileWriter("frequenciesPerInteger.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            for (int i = 0; i < factorizationFrequenciesPerNumberStrList.size(); i++) {
+                String entry = factorizationFrequenciesPerNumberStrList.get(i);
+                String str = "";
+                str += "(";
+                for (int j = 1; j < entry.length() - 1; j++) {
+                    str += entry.substring(j, j + 1);
+                }
+                str += ")";
+                bufferedWriter.write(str);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         for (int i = 0; i < factorizationFrequenciesPerNumber.size(); i++) {
             System.out.println(factorizationFrequenciesPerNumber.get(i));
         }
@@ -20,10 +49,10 @@ public class r_GetFactorizationFrequenciesForIntegers {
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public static ArrayList<ArrayList<Integer>> getFactorizationFrequenciesPerNumber(int upperBound) {
+    public static ArrayList<ArrayList<Integer>> getFactorizationFrequenciesPerNumber(int lowerBound, int upperBound) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
         ArrayList<Integer> compositeIntegers = new ArrayList<Integer>();
-        compositeIntegers = getCompositeIntegers(upperBound);
+        compositeIntegers = getCompositeIntegers(lowerBound, upperBound);
         for (int i = 0; i < compositeIntegers.size(); i++) {
             ArrayList<Integer> list = new ArrayList<Integer>();
             Factorizations test = new Factorizations(compositeIntegers.get(i));
@@ -34,9 +63,9 @@ public class r_GetFactorizationFrequenciesForIntegers {
         return res;
     }
 
-    public static ArrayList<Integer> getCompositeIntegers(int upperBound) {
+    public static ArrayList<Integer> getCompositeIntegers(int lowerBound, int upperBound) {
         ArrayList<Integer> res = new ArrayList<Integer>();
-        for (int i = 4; i <= upperBound; i++) {
+        for (int i = lowerBound; i <= upperBound; i++) {
             if (Factorizations.isPrime(i) == false)
                 res.add(i);
         }
