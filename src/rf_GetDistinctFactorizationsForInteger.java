4b49afd83de878196bc37d169b23b497ca672c83
Vignesh Nydhruva <nydhruva@wifi-10-45-198-78.wifi.berkeley.edu>
2024-03-01 00:42:00 -0800

    another commit

--- src/rf_GetDistinctFactorizationsForInteger.java
+++ src/rf_GetDistinctFactorizationsForInteger.java
@@ -5,15 +5,17 @@ public class rf_GetDistinctFactorizationsForInteger {
     public static void main(String[] args) {
 
         // input composite integer in the below line
-        int inputN = 100;
+        int inputN = 50 * (int) (Math.pow(3, 5));
         Factorizations obj = new Factorizations(inputN);
         ArrayList<ArrayList<Integer>> distinctFactorizations = new ArrayList<ArrayList<Integer>>();
         distinctFactorizations = obj.getDistinctFactorizations();
 
-        // prints the distinct factorizations of inputN
+        // IMPORTANT prints the distinct factorizations of inputN
         for (int i = 0; i < distinctFactorizations.size(); i++) {
             System.out.println(distinctFactorizations.get(i));
         }
+
+        System.out.println("Number of distinct factorizations for " + inputN + " is: " + distinctFactorizations.size());
 
     }
 
